28efd7b6dcb0fa1b2b80180284e7fcd9e254733c
Mike Frizzell <mike@mikefrizzell.com>
2023-04-11 21:07:00 -0500

    Updated form page

--- dashboard.py
+++ dashboard.py
@@ -2,11 +2,13 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import plotly
- 
+
+st.set_page_config(page_title="ROI Production", page_icon=":bar_chart:")
+
 # Connect to the SQLite database
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
- 
+
 # Create a table to store the form data
 c.execute('''CREATE TABLE IF NOT EXISTS form_data
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,17 +25,17 @@ c.execute('''CREATE TABLE IF NOT EXISTS form_data
              images_clouded INT,
              comment TEXT)''')
 conn.commit()
- 
+
 # Define a function to insert data into the form_data table
 def insert_data(date, name, calls, voicemail, call_time, request_type, number_done, pages_sent, time_spent, cds_created, images_clouded, comment):
-    c.execute("INSERT INTO form_data (date, name, calls, voicemail, call_time, request_type, number_done, pages_sent, time_spent, cds_created, images_clouded, comment VALUES (?, ?, ?)", (date, name, calls, voicemail, call_time, request_type, number_done, pages_sent, time_spent, cds_created, images_clouded, comment))
+    c.execute("INSERT INTO form_data (date, name, calls, voicemail, call_time, request_type, number_done, pages_sent, time_spent, cds_created, images_clouded, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (date, name, calls, voicemail, call_time, request_type, number_done, pages_sent, time_spent, cds_created, images_clouded, comment))
     conn.commit()
- 
+
 # Define a function to retrieve data from the form_data table
 def get_data():
     df = pd.read_sql_query("SELECT * FROM form_data", conn)
     return df
- 
+
 # List of user names
 user_names = [
     "Liam Patel",
@@ -53,48 +55,43 @@ roi_types = [
     "Law enforcement",
     "Regulatory",
 ]
-# Define a function to render the form page
-def render_form_page():
-    st.title("Production Form")
-    # Create input fields for data entry
-    date = st.date_input("Select date")
-    name = st.selectbox("Select your name", user_names)
-    calls = st.number_input("Calls - incoming and outgoing")
-    voicemail = st.number_input("Voicemails retreived")
-    call_time = st.number_input("Total phone time")
-    request_type = st.selectbox("Type of request", roi_types)
-    number_done = st.number_input("Number of requests completed")
-    pages_sent = st.number_input("Pages sent")
-    time_spent = st.number_input("Time spent (example 1 hour 15 minutes is 1.25)")
-    cds_created = st.number_input("Radiology CDs created")
-    images_clouded = st.number_input("Images clouded")
-    comment = st.text_input("Other/non productive time")
+st.header("Production")
+# Create input fields for data entry
+col1, col2 = st.columns(2)
 
-    # Display the data entered by the user
-    st.subheader("Please double-check your entries:")
-    st.write("Date:", date)
-    st.write("Name:", name)
-    st.write("Calls:", calls)
-    st.write("Voicemail:", voicemail)
-    st.write("Call time:", call_time)
-    st.write("Request type:", request_type)
-    st.write("Number of requests completed:", number_done)
-    st.write("Pages sent:", pages_sent)
-    st.write("Time spent:", time_spent)
-    st.write("Radiology CDs created:", cds_created)
-    st.write("Images clouded:", images_clouded)
-    st.write("Other/non-productive time:", comment)
+date = col1.date_input("Select date")
+name = col2.selectbox("Select your name", [" ", *user_names], index=0)
 
-    # Create a submit button
-    if st.button("Submit"):
+col1, col2, col3 = st.columns(3)
+
+calls = col1.number_input("Calls", help="Include both incoming and outgoing calls.", format=" %d", value=0, min_value=0)
+voicemail = col2.number_input("Voicemails retrieved", format=" %d", value=0, min_value=0)
+call_time = col3.number_input("Total phone time", help="Example: 1 hour 15 minutes is 1.25", format=" %d", value=0, min_value=0)
+
+col1, col2, col3 = st.columns(3)
+
+request_type = col1.selectbox("Type of request", [" ", *roi_types], index=0)
+number_done = col2.number_input("Number of requests completed", format=" %d", value=0, min_value=0)
+pages_sent = col3.number_input("Pages sent", format=" %d", value=0, min_value=0)
+
+col1, col2, col3 = st.columns(3)
+
+time_spent = col1.number_input("Time spent", help="Example: 1 hour 15 minutes is 1.25",format=" %d", value=0,min_value=0)
+cds_created = col2.number_input("Radiology CDs created", format=" %d", value=0, min_value=0)
+images_clouded = col3.number_input("Images clouded", format=" %d", value=0, min_value=0)
+
+comment = st.text_input("Other/non productive time", help="Mail, meetings, downtime, etc.")
+
+# Create a submit button
+if st.button("Submit"):
+    # Check if mandatory fields are filled
+    if not (date or name or request_type):
+        st.error("Please fill in all mandatory fields.")
+    else:
         # Insert the input data into the form_data table
         insert_data(date, name, calls, voicemail, call_time, request_type, number_done, pages_sent, time_spent, cds_created, images_clouded, comment)
         # Show a success message
         st.success("Data submitted successfully!")
- 
-     # Add a button to enter more data
-    if st.button("Enter additional data"):
-        # Clear the input fields
         st.experimental_rerun()
 
 # Define a function to render the dashboard page
@@ -104,18 +101,22 @@ def render_dashboard_page():
     df = get_data()
     # Show the data in a table
     st.dataframe(df)
- 
+
+# Define a function to render the form page
+def render_form_page():
+    st.write("Make sure to double check before submitting.")
+
 # Main Streamlit app
 def main():
-    st.set_page_config(page_title="ROI Production", page_icon=":bar_chart:")
     # Create a menu to switch between pages
-    menu = ["Form Page", "Dashboard Page"]
+    menu = ["Input production", "Dashboard"]
     choice = st.sidebar.selectbox("Select a page", menu)
+    
     # Render the selected page
-    if choice == "Form Page":
+    if choice == "Input production":
         render_form_page()
-    elif choice == "Dashboard Page":
+    elif choice == "Dashboard":
         render_dashboard_page()
- 
+
 if __name__ == "__main__":
     main()
