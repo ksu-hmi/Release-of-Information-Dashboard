7aba3d74c14697e113fa8c703885f8b7600ef279
Mike Frizzell <mpfrizzell@gmail.com>
2024-02-09 11:58:27 -0600

    Updates to dashboard

--- Home.py
+++ Home.py
@@ -2,4 +2,5 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.write("# ROI Production 2022")
+st.title("ROI Production 2022")
+st.subheader("Please use the links in the sidebar to enter production or to view the dashboard")
--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -4,10 +4,30 @@ import os
 
 st.set_page_config(page_title="Production Dashboard", page_icon=":bar_chart:")
 
-st.markdown("# Production Dashboard - 2022")
-st.sidebar.header("Dashboard")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
+st.title("Production Dashboard - 2022")
+
+# Read the CSV into a data frame
+df = pd.read_csv("data.csv")
+
+# Calculate the total number of releases
+total_releases = df["number_done"].sum()
+
+# Calculate the totals for each request_type
+totals_by_type = df.groupby("request_type")["number_done"].sum()
+
+# Calculate the total number of calls
+total_calls = df["calls", "voicemail"].sum()
+
+# Calculate the total pages sent
+total_pages = df["pages_sent"].sum()
+
+# Calculate images clouded and CDs created
+radiology = df["cds_created", "images_clouded"].sum()
+
+# Metrics bar
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Total releases", total_releases)
+col2.metric("Total calls", total_calls)
+col3.metric("Total pages sent", total_pages)
+col4.metric("Total images sent", radiology)
+
